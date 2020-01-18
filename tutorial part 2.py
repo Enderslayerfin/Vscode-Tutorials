9b11daa3ba271a322dd92038db6fae18929f0a9c
Enderslayerfin <60036950+Enderslayerfin@users.noreply.github.com>
2020-01-18 13:49:23 +0000

    Add files via upload
    part 2 of the tutorial

---
+++ tutorial part 2.py
@@ -0,0 +1,30 @@
+clear()
+print(" ")
+print(" ")
+
+#Main code
+#Part 1
+if first == 1:
+  part1 = "Fuck off"
+if first == 2:
+  part1 = "Shut the fuck up"
+if first == 3:  
+  part1 = "Piss off"
+if first == 4:
+  part1 = "Get out"
+if first == 5:
+  part1 = "It's RAWWW"
+if first == 6:
+  part1 = "It's BURNT"
+if first == 7:
+  part1 = "What the FUCK are you doing"
+if first == 8:      
+  part1 = "Move your ass"
+if first == 9:
+  part1 = "More SAUCE"
+if first == 10:
+  part1 = "Wake up"                   
+if first == 11:
+  part1 = "Get a grip"
+if first == 12:
+  part1 = "Gimme your jacket"
